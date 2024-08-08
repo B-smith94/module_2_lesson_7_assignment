5e3c1471e05d83964e61927160dc48814f8060ba
b-smith94 <byrondsmith9@gmail.com>
2024-08-08 14:01:38 -0400

    Set variable for input temperature in Fahrenheit; created a function using Try and Except blocks to convert to Celsius; added Else block to print out conversion in a user-friendly way; added Finally block to thank the user for using this application.

---
+++ exceptional_weather.py
@@ -0,0 +1,18 @@
+#Task 1
+temperature_in_fahrenheit = input("Temperature in Fahrenheit: ")
+
+#Task 2
+def temperature_conversion(temperature):
+    try:
+        celsius = round(((int(temperature) - 32) * 5/9))
+    except ValueError:
+        print("Error: must input temperature as a whole number.")
+#Task 3        
+    else:
+        print(f"{temperature}° Fahrenheit is {celsius}° Celsius.")
+#Task 4    
+    finally:
+        print("Thank you for using this weather forcast application!")
+
+
+temperature_conversion(temperature_in_fahrenheit)
